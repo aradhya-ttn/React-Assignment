96642ccdf3ebaf0653bdcc96deff3ae3ba56c4fe
aradhya-ttn <aradhya.patel@tothenew.com>
2020-05-12 17:18:44 +0530

    basic redux

--- my-burger-app/src/store/reducer.js
+++ my-burger-app/src/store/reducer.js
@@ -1,14 +1,13 @@
-import * as actionTypes from './actions'
-
+import * as actionTypes from './actions';
 const initialState = {
     ingredients : {
-        salad:0,
-        meat:0,
-        bacon:0,
-        cheese:0
-    },
+        salad : 0,
+        bacon : 0,
+        cheese : 0,
+        meat : 0
+    },  
     totalPrice: 15,
-};
+}
 
 const INGREDIENT_PRICES = {
     salad: 25,
@@ -17,31 +16,29 @@ const INGREDIENT_PRICES = {
     bacon: 35
 };
 
-
-const reducer = (state = initialState, action) =>{
-    switch (action.type){
+const reducer = (state = initialState,action) => {
+    switch(action.type){
         case actionTypes.ADD_INGREDIENT:
             return{
                 ...state,
                 ingredients: {
                     ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName]+1
+                    [action.ingredientName]:state.ingredients[action.ingredientName]+1
                 },
                 totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            };
-        case actionTypes. REMOVE_INGREDIENT:
+            }
+        case actionTypes.REMOVE_INGREDIENT:
             return{
                 ...state,
                 ingredients: {
                     ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName]-1
+                    [action.ingredientName]:state.ingredients[action.ingredientName]-1
                 },
-                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredients]
-            }; 
+                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+            }
         default:
             return state;
     }
-    
-};
+}
 
-export  default reducer;
+export default reducer;
