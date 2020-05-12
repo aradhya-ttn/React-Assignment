24b01239df205e3856c866fe4617e7e177edfb6b
aradhya-ttn <aradhya.patel@tothenew.com>
2020-05-12 10:24:41 +0530

    a

--- my-burger-app/src/axios-orders.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL:'https://react-my-burger-bootcamp.firebaseio.com/'
-});
-
-export default instance;
--- my-burger-app/src/components/UI/Spinner/Spinner.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import classes from './Spinner.module.css';
-
-const spinner = () => (
-    <div className={classes.ldsroller}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>Loading</div> 
-);
-export default spinner;
